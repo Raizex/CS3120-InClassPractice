32efe735ea99669ba3b8639d23e0f6d75e827fce
Justin Huffman <jwhuffman@gmail.com>
2019-02-20 16:25:56 -0700

    Broke out data by marital status.

--- 2/banking.py
+++ 2/banking.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('banking.csv')
-married = data.loc[data['marital'] == 'married'].loc[:, ['marital','y']]
-single = data.loc[data['marital'] == 'single'].loc[:, ['marital','y']]
-print(married)
-print(single)
+
+for status in data['marital'].unique():
+    marital = data.loc[data['marital']==status]['y'].value_counts()
+    print(status)
+    print(marital,'\n')
