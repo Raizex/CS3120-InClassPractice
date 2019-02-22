4e4c6233e9baceaf518c9f83c398db6583e201f0
Justin Huffman <jwhuffman@gmail.com>
2019-02-21 23:18:20 -0700

    Fixed bar graph.

--- 2/banking.py
+++ 2/banking.py
@@ -13,9 +13,10 @@ for status in data['marital'].unique():
 print(marital_data,'\n')
 
 frequencies = marital_data.iloc[1] / (marital_data.iloc[0] + marital_data.iloc[1])
-marital_frequencies = pd.DataFrame(frequencies, columns=['f']).T
+marital_frequencies = pd.DataFrame(frequencies, columns=['1'])
+marital_frequencies['0'] = 1 - marital_frequencies['1']
 
 print(marital_frequencies)
 
-marital_frequencies.T.plot.bar()
+marital_frequencies.plot.bar(stacked=True)
 marital_data.T.plot.bar(stacked=True)
